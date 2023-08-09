fb9a8f368f1af50ed48c4c69ec339f86fa3fbb26
Scholtz <ludko2@gmail.com>
2023-08-09 17:06:38 +0200

    cicd update from 3.16.3 to 3.17.0

---
+++ compose-algorand-kmd-mainnet-extended-3.17.0.sh
@@ -0,0 +1,16 @@
+ver=3.17.0
+image=$ver-stable
+docker build -t scholtz2/algorand-kmd-mainnet-extended:$image -f Dockerfile --progress=plain --build-arg ALGO_VER=$ver --build-arg ALGO_BASE=$image . || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "failed to build";
+	exit 1;
+fi
+
+docker push scholtz2/algorand-kmd-mainnet-extended:$ver-stable || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "failed to build";
+	exit 1;
+fi
+
