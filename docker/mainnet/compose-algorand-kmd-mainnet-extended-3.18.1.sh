c92f00777e8d60c149daf9c22e66d390921050ed
Scholtz <ludko2@gmail.com>
2023-10-03 18:08:37 +0200

    cicd update from 3.18.0 to 3.18.1

---
+++ docker/mainnet/compose-algorand-kmd-mainnet-extended-3.18.1.sh
@@ -0,0 +1,33 @@
+ver=3.18.1
+image=$ver-stable
+Application="algorand-participation-server"
+dockerImage="scholtz2/algorand-kmd-mainnet-extended"
+version=$image
+
+gitVer=`git rev-parse HEAD`
+
+echo "{\"applicationName\":\"$Application\",\"buildNumber\":\"$ver\",\"dllVersion\":\"\",\"buildTime\":\"$time\",\"sourceVersion\":\"$gitVer\",\"dockerImage\":\"$dockerImage:$version\",\"dockerImageVersion\":\"$version\"}"> "version.json" 
+cat "version.json" 
+
+docker build -t scholtz2/algorand-kmd-mainnet-extended:$image -f Dockerfile --progress=plain --build-arg ALGO_VER=$ver --build-arg ALGO_BASE=$image ../../ || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "failed to build";
+	exit 1;
+fi
+
+docker push scholtz2/algorand-kmd-mainnet-extended:$ver-stable || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "failed to push algorand-kmd-mainnet-extended";
+	exit 1;
+fi
+
+
+docker tag scholtz2/algorand-kmd-mainnet-extended:$ver-stable scholtz2/algorand-participation-mainnet-extended:$ver-stable
+docker push scholtz2/algorand-participation-mainnet-extended:$ver-stable || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "failed to push algorand-participation-mainnet-extended";
+	exit 1;
+fi
